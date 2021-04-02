060028abd18857e197f06e41346c24471ad06ec7
Roma Dod <stalkeroma1@mail.ru>
2021-04-03 00:04:11 +0300

    Update ViewController (#10)
      - add textFields
      - add uiswitch
      - add constraints
      - fix  GalleryCollectionViewCell
      - update logic for userdefaults in ViewController
      - update logic for userdefaults in ViewController
      - configure constraints via XIB

--- SimpleApp/GalleryCollectionViewCell.swift
+++ SimpleApp/GalleryCollectionViewCell.swift
@@ -1,11 +1,51 @@
 import UIKit
+import Hex
+import Kingfisher
 
 class GalleryCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(cgColor: CGColor(red: 0, green: 2, blue: 3, alpha: 0.3))
+        self.translatesAutoresizingMaskIntoConstraints = false
+        addSubView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     let imageView: UIImageView = {
         let imageV = UIImageView()
         imageV.translatesAutoresizingMaskIntoConstraints = false
         return imageV
     }()
+
+    let label: UILabel = {
+        let labelV = UILabel()
+        labelV.translatesAutoresizingMaskIntoConstraints = false
+        labelV.numberOfLines = 0
+        return labelV
+    }()
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.isHidden = false
+        label.text = nil
+        imageView.image = nil
+    }
+
+    var splashImage: SplashImage? {
+        didSet {
+            label.text = splashImage?.altDescription
+            label.isHidden = label.text == nil
+            guard let splashImage = splashImage, let urlRegular = URL(string: splashImage.urls.regular) else {
+                imageView.image = nil
+                return
+            }
+            label.backgroundColor = UIColor(hex: splashImage.color)
+            imageView.kf.setImage(with: urlRegular)
+        }
+    }
 
     private func addSubView() {
         addSubview(imageView)
@@ -14,15 +54,5 @@ class GalleryCollectionViewCell: UICollectionViewCell {
         imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 2).isActive = true
         imageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 2).isActive = true
         imageView.rightAnchor.constraint(equalTo: rightAnchor, constant: 2).isActive = true
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor(cgColor: CGColor(red: 0, green: 2, blue: 3, alpha: 0.3))
-        addSubView()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -2,34 +2,142 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet private(set) var label: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var saveCredsSwitch: UISwitch!
+    @IBOutlet weak var savePasswordLabel: UILabel!
+
+    private enum Constants {
+        static let userNameKey = "userNameKey"
+        static let passwordKey = "passwordKey"
+        static let isCheckedKey = "isCheckedKey"
+        static let username = "user"
+        static let password = "password"
+    }
+
+    private var username: String?
+    private var password: String?
+
+    @IBAction func saveCredsSwitchChanged() {
+        removeNamePasswordIfNeeded()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordTextField.isSecureTextEntry = true
+        label.text = "SimpleApp"
+        label.textAlignment = .center
+        button.setTitle("Sign In", for: .normal)
+        savePasswordLabel.text = "Save credentials"
 
-        label.text = "label"
+        usernameTextField.addTarget(
+            self,
+            action: #selector(updateUsernameProperty),
+            for: .editingChanged
+        )
+        passwordTextField.addTarget(
+            self,
+            action: #selector(updatePasswordProperty),
+            for: .editingChanged
+        )
+        restoreNamePasswordIfNeeded()
+        restoreSwitchState()
     }
-    @IBAction func buttonPressed(_ sender: UIButton) {
+
+    private func removeNamePasswordIfNeeded() {
+        let expression = !saveCredsSwitch.isOn
+        if expression {
+            UserDefaults.standard.removeObject(forKey: Constants.userNameKey)
+            UserDefaults.standard.removeObject(forKey: Constants.passwordKey)
+        }
+    }
+
+    private func saveNamePasswordIfNeeded() {
+        let expression = saveCredsSwitch.isOn
+        if expression {
+            UserDefaults.standard.set(username, forKey: Constants.userNameKey)
+            UserDefaults.standard.set(password, forKey: Constants.passwordKey)
+        }
+    }
+
+    private func restoreNamePasswordIfNeeded() {
+        if let isCheckedKey = UserDefaults.standard.value(forKey: Constants.isCheckedKey) as? Bool, isCheckedKey {
+            restoreStoredNamePassword()
+        }
+    }
+
+    func restoreStoredNamePassword() {
+        usernameTextField.text = UserDefaults.standard.value(forKey: Constants.userNameKey) as? String
+        passwordTextField.text = UserDefaults.standard.value(forKey: Constants.passwordKey) as? String
+        updateUsernameProperty()
+        updatePasswordProperty()
+    }
+
+    func restoreSwitchState() {
+        saveCredsSwitch.isOn = (UserDefaults.standard.value(forKey: Constants.isCheckedKey) as? Bool) ?? false
+    }
+
+    private func showHomeController() {
         let tabBarVC = UITabBarController()
         let feedVC = UINavigationController(
             rootViewController: FeedViewController(fetcher: Manager.shared)
         )
-        let galleryVC = UINavigationController(rootViewController: GalleryViewController(fetcher: SplashImageFetcher()))
-
+        let galleryVC = UINavigationController(
+            rootViewController: GalleryViewController(fetcher: SplashImageFetcher())
+        )
         feedVC.title = "Feed"
         galleryVC.title = "Gallery"
-
         tabBarVC.setViewControllers([feedVC, galleryVC], animated: true)
-
         guard let items = tabBarVC.tabBar.items else {
             return
         }
-
         let images = ["house", "star"]
-
         for x in 0..<items.count {
             items[x].image = UIImage(systemName: images[x])
         }
         tabBarVC.modalPresentationStyle = .fullScreen
         present(tabBarVC, animated: true, completion: nil)
+    }
+
+    private func showOopsAlert() {
+        let alert = UIAlertController(title: "Oops!", message: "Your credentials are wrong", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    private func saveSwitchState() {
+        UserDefaults.standard.set(saveCredsSwitch.isOn, forKey: Constants.isCheckedKey)
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if username == Constants.username && password == Constants.password {
+            saveNamePasswordIfNeeded()
+            saveSwitchState()
+            showHomeController()
+        } else {
+            showOopsAlert()
+        }
+    }
+
+    @objc func updateUsernameProperty() {
+        print(usernameTextField.text as Any)
+        username = usernameTextField.text
+    }
+
+    @objc func updatePasswordProperty() {
+        print(passwordTextField.text as Any)
+        password = passwordTextField.text
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(
+        _ textField: UITextField
+    ) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
